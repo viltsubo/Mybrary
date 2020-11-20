c9e1d0e525d1b1c05aaeeb78cd882bd182b3e485
viltsubo <majis.ville@gmail.com>
2020-11-20 19:29:04 +0200

    Episode 5, left at 25:38

--- routes/authors.js
+++ routes/authors.js
@@ -31,8 +31,7 @@ router.post('/', async (req, res) => {
   })
   try {
     const newAuthor = await author.save()
-    // res.redirect(`authors/${newAuthor.id}`)
-    res.redirect(`authors`)
+    res.redirect(`/authors/${newAuthor.id}`)
   } catch {
     res.render('authors/new', {
       author: author,
@@ -40,5 +39,53 @@ router.post('/', async (req, res) => {
     })
   }
 })
+
+router.get('/:id', (req, res) => {
+  res.send('Show Author ' + req.params.id)
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error creating Author'
+      })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
+  }
+})
+
 
 module.exports = router
